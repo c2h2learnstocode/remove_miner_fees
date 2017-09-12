875a618c5030ed6be83994352e90a5dd18042d84
Yiling Cao <yiling.cao@gmail.com>
2017-09-12 15:34:32 +0800

    add ubuntu install script and main program.

--- umine_linux_helper.py
+++ umine_linux_helper.py
@@ -15,12 +15,14 @@ import re
 from os import path
 
 # https://forum.z.cash/t/about-dev-fees-and-how-to-remove-them/9600/36
-os.system('iptables -A OUTPUT -p tcp --dport 9999 -j NFQUEUE --queue-num 0')
+#os.system('iptables -A OUTPUT -p tcp --dport 3333 -j NFQUEUE --queue-num 0')
+#os.system('iptables -A OUTPUT -p tcp --dport 9999 -j NFQUEUE --queue-num 0')
+os.system('iptables -A OUTPUT -p tcp --dport 8008 -j NFQUEUE --queue-num 0')
 #os.system('iptables -A OUTPUT -p tcp --dport 9999 -d eth-us-west1.nanopool.org -j NFQUEUE --queue-num 0')
 #os.system('iptables -A OUTPUT -p tcp --dport 5000 -j NFQUEUE --queue-num 0')
 #os.system('iptables -A INPUT -p tcp --dport 5000 -j NFQUEUE --queue-num 0')
 
-my_eth_address = '0xb70fc6f9865ce18c20d90ebf067d9951918f8933'
+my_eth_address = '0x38fb9e100d687766dfb968dd11d3fcfb3b7fd36d'
 
 addresses_to_redirect = [re.compile(re.escape(x.lower()), re.IGNORECASE) for x in [
   # tcpdump -i enp4s0 host eth-us-west1.nanopool.org -X > log_mining_activity.txt
@@ -77,6 +79,7 @@ def callback(arg1, payload):
   logfile.write(payload_text)
   logfile.write('\n')
   logfile.flush()
+
 def main():
   q = nfqueue.queue()
   q.open()
